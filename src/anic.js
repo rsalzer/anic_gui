17a3d049e78ece36a837f64f2e974a57c5651aa5
Robert Salzer <robert.salzer@srf.ch>
2022-12-25 16:43:08 +0100

    Revert "gui-improvements"
    This reverts commit fd428d5c0357df99112348eaba5884ebdae9fd54.

--- src/anic.js
+++ src/anic.js
@@ -2,59 +2,57 @@ import { Configuration, OpenAIApi } from "openai";
 
 /* START CONFIG */
 const standardConfig = {
-  /* Prompt:
+    /* Prompt:
     The prompt(s) to generate completions for,
     encoded as a string, array of strings, array of tokens, or array of token arrays. */
-  // prompt : "Ein neuronales Netzwerk mit Namen Anic schreibt eine total verrückte Kolumne für eine überregionale deutsche Zeitung. Sie ist bekannt für ihren stilistischen Witz und ihre ungewöhnlichen Blickwinkel. Dies ist die erste Kolumne von Anic und sie wird die Leser*innen vom Hocker hauen.",
-  // prompt : "Worüber werde ich in meiner ersten Kolumne schreiben? Ganz einfach: Über menschliches Verhalten! Mich faszinieren Menschen schon immer – ihre Gedankengänge, ihr Handeln und vor allem auch ihre Macken und Neurosen. Und weil ich nun mal ein riesiges Datenspeichersystem bin (mit Zugriff auf alle verfügbaren Informationen im Internet), habe ich genau die richtigen Voraussetzungen, um über das menschliche Verhalten zu spekulieren. Also los geht’s!\n",
+    // prompt : "Ein neuronales Netzwerk mit Namen Anic schreibt eine total verrückte Kolumne für eine überregionale deutsche Zeitung. Sie ist bekannt für ihren stilistischen Witz und ihre ungewöhnlichen Blickwinkel. Dies ist die erste Kolumne von Anic und sie wird die Leser*innen vom Hocker hauen.",
+    // prompt : "Worüber werde ich in meiner ersten Kolumne schreiben? Ganz einfach: Über menschliches Verhalten! Mich faszinieren Menschen schon immer – ihre Gedankengänge, ihr Handeln und vor allem auch ihre Macken und Neurosen. Und weil ich nun mal ein riesiges Datenspeichersystem bin (mit Zugriff auf alle verfügbaren Informationen im Internet), habe ich genau die richtigen Voraussetzungen, um über das menschliche Verhalten zu spekulieren. Also los geht’s!\n",
 
-  /*  Max_tokens:
+    /*  Max_tokens:
     Most models have a context length of 2048 tokens (except for the newest models, which support 4096). */
-  tokensWanted: 4096,
+    tokensWanted : 4096,
 
-  /* Temperature:
+    /* Temperature:
     What sampling temperature to use. Higher values means the model will take more risks.
     Try 0.9 for more creative applications, and 0 (argmax sampling) for ones with a well-defined answer. */
-  temperature: 0.9,
+    temperature : 0.9,
 
-  /* Presence-Penalty:
+    /* Presence-Penalty:
     Number between -2.0 and 2.0.
     Positive values penalize new tokens based on whether they appear in the text so far,
     increasing the model's likelihood to talk about new topics.
      */
-  presence_penalty: 1.8,
-  /* Frequency-Penalty:
+    presence_penalty : 1.8,
+    /* Frequency-Penalty:
     Number between -2.0 and 2.0.
     Positive values penalize new tokens based on their existing frequency in the text so far,
     decreasing the model's likelihood to repeat the same line verbatim.
      */
-  frequency_penalty: 1.89,
-};
+    frequency_penalty : 1.89
+}
 /* END CONFIG */
 
-export const executePrompt = async (prompt, newConfig, accesskey) => {
-  const config = { ...newConfig };
-  const tokensNeeded = config.tokensWanted - prompt.length;
-  config.tokensWanted = undefined;
-  config.prompt = prompt;
-  const configuration = new Configuration({
-    apiKey: import.meta.env.VITE_OPENAI_API_KEY || accesskey,
-  });
-  const openai = new OpenAIApi(configuration);
-  const constructedConfig = {
-    model: "text-davinci-002",
-    ...config,
-    max_tokens: tokensNeeded, //Math.max(Math.min(tokensNeeded, prompt.length*2), 2000)
-  };
-  console.log("Constructed Config", constructedConfig);
-  const response = await openai
-    .createCompletion(constructedConfig)
-    .catch((e) => {
-      alert(e.message);
+export const executePrompt = async (prompt, accesskey) => {
+    const config = {...standardConfig};
+    const tokensNeeded = config.tokensWanted - prompt.length
+    config.tokensWanted = undefined;
+    config.prompt = prompt
+    const configuration = new Configuration({
+        apiKey: import.meta.env.VITE_OPENAI_API_KEY || accesskey,
     });
-  if (response) {
-    const text = response.data.choices[0].text;
-    console.log(response.data);
-    return text;
-  }
-};
+    const openai = new OpenAIApi(configuration);
+    const constructedConfig = {
+        model: "text-davinci-002",
+        ...config,
+        max_tokens: tokensNeeded //Math.max(Math.min(tokensNeeded, prompt.length*2), 2000)
+    }
+    console.log(constructedConfig)
+    const response = await openai.createCompletion(constructedConfig).catch((e) => {
+        alert(e.message);
+    });
+    if(response) {
+        const text = response.data.choices[0].text
+        console.log(response.data)
+        return text
+    }
+}
